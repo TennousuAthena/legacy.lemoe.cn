6104611293288d21c8473280428319841da348a9
天王州アテネ <qingcaomc@gmail.com>
2021-05-30 19:20:24 +0800

    更新get-version

--- get-version.py
+++ get-version.py
@@ -2,10 +2,26 @@
 # -*- coding: UTF-8 -*-
 import requests
 import json
+# Github 仓库名，现使用package.json->gh
+#repo = "TennousuAthena/tennousuAthena.github.io"
+
+file_handle = open("package.json")
+lines = file_handle.read()
+package = json.loads(lines)
+file_handle.close()
+
+repo = package['gh']
+
+api_base = "https://api.github.com/repos/" + repo
+
 print("[Get-Version]Fetching...")
 # 获取所有的commits
-res = requests.get('https://api.github.com/repos/TennousuAthena/tennousuAthena.github.io/commits').json()[0]
+res = requests.get(api_base + '/commits').json()[0]
 author = res['commit']['author']
+
+# 获取commits数
+commits = requests.get(api_base + '/stats/contributors').json()[0]['total']
+
 result = {
     'author': author['name'],
     'date': author['date'],
@@ -13,12 +29,15 @@ result = {
     'commit_id': res['sha'][0:7],
     'commit_sha': res['sha'],
     'verification': res['commit']['verification']['verified'],
+    'total': commits,
     'url': res['html_url']
 }
+res_json = json.dumps(result)
+print(res_json)
+
 print("[Get-Version]Saving...")
-json = json.dumps(result)
 file_handle = open('public/version.json', mode='w')
-file_handle.write(json)
+file_handle.write(res_json)
 file_handle.close()
 
 print("[Get-Version]Done!")
