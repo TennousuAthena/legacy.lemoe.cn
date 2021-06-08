cc698af01cbb64b92075db68ff3d2705ef6afeb4
天王州アテネ <qingcaomc@gmail.com>
2021-06-08 20:09:26 +0800

    血的教训：永远不要以为自己能驾驭pjax

--- themes/yun/source/js/pjax.js
+++ themes/yun/source/js/pjax.js
@@ -24,13 +24,37 @@ function initPjax() {
 function onPjaxSuccess() {
   isHome();
   // Yun.utils.renderKatex();
-  console.log(window.twicomment)
-  
-  window.twicomment.el = '#tcomment'
-  twikoo.init(window.twicomment)
+  loadScript("https://cdn.jsdelivr.net/npm/twikoo@latest/dist/twikoo.all.min.js", function () {
+    console.log(window.twicomment)
+    twikoo.init(window.twicomment)
+  })
+
 }
 
+
+window.onload = function () {
+  onPjaxSuccess()
+}
 
 document.addEventListener("DOMContentLoaded", initPjax);
 document.addEventListener("DOMContentLoaded", isHome);
 document.addEventListener("pjax:success", onPjaxSuccess);
+
+function loadScript(url, callback){
+  var script = document.createElement ("script")
+  script.type = "text/javascript";
+  if (script.readyState){ //IE
+      script.onreadystatechange = function(){
+          if (script.readyState == "loaded" || script.readyState == "complete"){
+              script.onreadystatechange = null;
+              callback();
+          }
+      };
+  } else { //Others
+      script.onload = function(){
+          callback();
+      };
+  }
+  script.src = url;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
